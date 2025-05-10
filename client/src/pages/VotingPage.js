a8710ad24e0aab14dd14bbc0cf20a6d56140959f
nks-16 <2022cs_karthikeyas_b@nie.ac.in>
2025-05-10 09:09:49 +0530

    destiny reached

--- client/src/pages/VotingPage.js
+++ client/src/pages/VotingPage.js
@@ -601,7 +601,7 @@ const VotingPage = () => {
                       <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                       </svg>
-                      VOTE YES
+                      YES
                     </div>
                   </button>
                   <button
@@ -618,7 +618,7 @@ const VotingPage = () => {
                       <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                       </svg>
-                      VOTE NO
+                      NO
                     </div>
                   </button>
                 </div>
